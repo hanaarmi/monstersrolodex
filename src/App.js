56dd08e496ca3320711f7b71537283a97c8a1a5f
Jungkon Kim <primer79@gmail.com>
2021-07-12 02:03:17 +0900

    code after 33. CardList Component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { CardList } from './components/card-list/card-list.component'
 import './App.css'
 
 class App extends Component {
@@ -18,9 +19,11 @@ class App extends Component {
 	render() {
 		return (
 			<div className="App">
-				{this.state.family.map((p) => (
-					<h1 id={p.id}>{p.name}</h1>
-				))}
+				<CardList>
+					{this.state.family.map((p) => (
+						<h1 id={p.id}>{p.name}</h1>
+					))}
+				</CardList>
 			</div>
 		)
 	}
---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import './card-list.styles.css'
+
+export const CardList = (props) => {
+	return <div class="card-list">{props.children}</div>
+}
